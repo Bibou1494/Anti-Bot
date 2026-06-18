6cb410d936231784ef2f47e1b09c216c4b43c7a2
Bibou1494 <gdmcplayer@proton.me>
2026-06-18 20:58:30 +0200

    Added the base project

---
+++ main.js
@@ -0,0 +1,34 @@
+const { Client, Events, GatewayIntentBits, PermissionFlagsBits } = require('discord.js');
+require('dotenv').config();
+
+const client = new Client({ intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildMessages, GatewayIntentBits.MessageContent] });
+
+client.once(Events.ClientReady, (readyClient) => {
+	console.log(`Ready! Logged in as ${readyClient.user.tag}`);
+});
+
+client.on('messageCreate', async (message) => {
+	if (message.author.bot) return;
+
+	console.log(`[#${message.channel.name}] ${message.author.username}: ${message.content}`);
+
+	if (message.channel.name === 'antibot') {
+		console.log(`Bot account detected: ${message.author.tag}`);
+
+		if (message.guild.members.me.permissions.has(PermissionFlagsBits.BanMembers)) {
+			try {
+				await message.member.ban({ reason: 'Bot account detected' });
+				console.log(`Banned user: ${message.author.username}`);
+			}
+			catch (error) {
+				console.error(`Error when banning: ${error.message}`);
+			}
+		}
+		else {
+			console.error('The bot doesn\'t have permission: Banning');
+		}
+		return;
+	}
+});
+
+client.login(process.env.TOKEN);
